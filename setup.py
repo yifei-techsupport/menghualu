350857dd38c395a3c1d66313fa661b27cca245b4
yifei-techsupport <109063333+yifei-techsupport@users.noreply.github.com>
2022-10-17 21:25:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup, find_packages
+import setuptools
 
-str_version = '1.0.0'
-
-setup(name='yifeitechsupport',
-	  version=str_version,
-	  description='bofangliang tools for yifei',
-	  author='yifei_tech_support',
-	  author_email='zufang.st@gmail.com',
-	  license='feimi',
-	  url = 'https://github.com/yifei-techsupport/yifei-tech-support.git'
-	  packages=find_packages(),
-	  zip_safe=False,
-	  python_requires='>3')
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="yifeitechsupport",
+    version="0.0.1",
+    author="yifei_tech_support",
+    author_email="zufang.st@gmail.com",
+    description="tools for yifei",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yifei-techsupport/menghualu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
